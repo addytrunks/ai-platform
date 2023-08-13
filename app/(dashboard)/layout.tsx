709a8a9d9cb2c257422315cd8f784d97a7d41af2
Adhithya Srivatsan <addytrunks@gmail.com>
2023-08-13 12:26:44 +0530

    feat: dashboard layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import '@/app/globals.css'
+import Navbar from '@/components/navbar'
+import Sidebar from '@/components/sidebar'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className='h-full relative'>
+        <div className='hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900'>
+            <Sidebar/>
+        </div>
+        <main className='md:pl-72'>
+            <Navbar/>
+            {children}
+        </main>
+    </div>
+  )
+}
