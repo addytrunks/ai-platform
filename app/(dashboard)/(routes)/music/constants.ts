9236aa3c6121f08231dcc4f1c4377018ad21969c
Adhithya Srivatsan <addytrunks@gmail.com>
2023-08-13 14:44:11 +0530

    feat: music entity

---
+++ app/(dashboard)/(routes)/music/constants.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+    prompt:z.string().min(5,{
+        message:'Music prompt must alteast contain 5 character(s)'
+    })
+})
---
+++ app/api/music/route.ts
@@ -0,0 +1,34 @@
+import { replicate } from "@/lib/replicate";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { prompt } = body;
+    const { userId } = auth();
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!replicate)
+      return new NextResponse("No Replicate API key configured", {
+        status: 500,
+      });
+
+    if (!prompt) return new NextResponse("Prompt is required", { status: 400 });
+
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    );
+
+    return NextResponse.json(response)
+  } catch (error) {
+    console.log("[IMAGE_ERROR]", error);
+    return NextResponse.json("Internal Server Error", { status: 500 });
+  }
+};
