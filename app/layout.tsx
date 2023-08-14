3ea61727a5f7745014909d6538d22d7d81ab0d60
Adhithya Srivatsan <addytrunks@gmail.com>
2023-08-14 20:27:12 +0530

    feat: toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ModalProvider from '@/components/modal-provider'
+import {Toaster} from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <Toaster/>
           <ModalProvider/>
           {children}
         </body>
